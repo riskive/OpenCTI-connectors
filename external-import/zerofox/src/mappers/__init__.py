893c162cf6ec21b495dda98bc81cc5298485312a
Diego Ramirez <dramirez@zerofox.com>
2024-05-07 23:07:21 -0400

    Add ransomware data feed
    add ransomware feed mapper and update map_to_stix dictionary

--- external-import/zerofox/src/mappers/__init__.py
+++ external-import/zerofox/src/mappers/__init__.py
@@ -2,11 +2,12 @@ from datetime import datetime
 from zerofox.app.endpoints import CTIEndpoint
 from typing import Any
 from mappers.malwareToMalware import malware_to_malware
-
+from mappers.ransomwareToMalware import ransomware_to_malware
 
 def threat_feed_to_stix(feed: Any):
     return {
         CTIEndpoint.Malware: malware_to_malware,
+        CTIEndpoint.Ransomware: ransomware_to_malware,
 
 
     }.get(feed)
